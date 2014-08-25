602c8e9f4d4b5b313dc5d17ee42a5fbfcc8cbcd6
agustin3a94 <agustin3a94@gmail.com>
2014-08-24 19:56:28 -0600

    Reset 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'http://c3a-pinteresting.herokuapp.com/' }
 
   #sets papeclip to upload images to amazon s3
-
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
